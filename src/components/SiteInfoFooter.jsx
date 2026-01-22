7b16ed775998a828286cd0151a7f0d9b80665748
MichaelGalloway404 <michael.d.galloway404@gmail.com>
2026-01-22 09:19:17 -0600

    testing out style behavior

--- src/components/SiteInfoFooter.jsx
+++ src/components/SiteInfoFooter.jsx
@@ -9,6 +9,17 @@ function SiteInfoFooter(props) {
       <h2 className={`${className} font-xl`}>Books are an entry way that only the individual can experiance,
          though the ground may have been traversed by another just as life the experiance is yours.
       </h2>
+      <ul>
+        <li>
+          <p>test for inheritance of styles</p>
+        </li>
+        <li>
+          <p>test for inheritance of styles</p>
+        </li>
+        <li>
+          <p>test for inheritance of styles</p>
+        </li>
+      </ul>
     </div>
   );
 }
