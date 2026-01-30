028bdc2d9dfa206d959cc71d197ccdf0ee969bb1
MichaelGalloway404 <michael.d.galloway404@gmail.com>
2026-01-30 09:22:23 -0600

    moved comoponets that will now be pages into page folder

--- api/phrases.js
+++ api/phrases.js
@@ -1,26 +1,35 @@
-import { Pool } from "pg";
+import { Pool } from "pg"; // PostgreSQL tools, and Pool manages a pool of database connections
+// Instead of opening a new DB connection every request
 
+// Keep DB credentials out of code
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
+// Next.js / serverless setup, runs every time a request hits this endpoint
 export default async function handler(req, res) {
+  // client is asking for data
   if (req.method === "GET") {
     try {
       const result = await pool.query("SELECT name, phrase FROM phrases ORDER BY id DESC");
-      res.status(200).json(result.rows);
+      // send back the querry, 200 ok, and client gets data [{"name":name, "phrase":phrase},{},...] newest entries 1st
+      res.status(200).json(result.rows); 
     } catch (err) {
       console.error(err);
       res.status(500).json({ error: "Database error" });
     }
-  } else if (req.method === "POST") {
-    const { name, phrase } = req.body;
+  } 
+  // client sends data 
+  else if (req.method === "POST") {
+    const { name, phrase } = req.body; // exstract data
     try {
       await pool.query("INSERT INTO phrases (name, phrase) VALUES ($1, $2)", [name, phrase]);
-      res.status(200).json({ ok: true });
+      res.status(200).json({ ok: true }); // worked
     } catch (err) {
       console.error(err);
       res.status(500).json({ error: "Database error" });
     }
-  } else {
+  } 
+  // 
+  else {
     res.status(405).json({ error: "Method not allowed" });
   }
 }
--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SecondPage from "./pages/SecondPage";
-import LoginPage from "./components/LoginPage";
+import LoginPage from "./pages/LoginPage";
 import ProtectedRoute from "./components/ProtectedRoute";
-import BookSearch from "./components/BookSearch"
+import BookSearch from "./pages/BookSearch"
 
 function App() {
   return (
     <Routes>
 
       <Route path="/" element={<Home />} />
-      
+
       <Route path="/search" element={<BookSearch />} />
 
       <Route path="/login" element={<LoginPage />} />
