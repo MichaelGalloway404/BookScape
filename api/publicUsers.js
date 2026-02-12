aa68e8c42fd04c283458349d05ad264368cf5f65
MichaelGalloway404 <michael.d.galloway404@gmail.com>
2026-02-12 11:59:58 -0600

    API fix for now just showing any profile

--- api/publicUsers.js
+++ api/publicUsers.js
@@ -3,14 +3,15 @@ import { Pool } from "pg";
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
 export default async function handler(req, res) {
-
-  // GET: user info
+    // GET: user info
   if (req.method === "GET") {
     try {
       const result = await pool.query(
         "SELECT username, book_order_json FROM users WHERE private = TRUE"
       );
-      return res.status(200).json(result.rows[0]);
+
+      // Return all rows, not just the first one
+      return res.status(200).json(result.rows);
     } catch (err) {
       console.error(err);
       return res.status(500).json({ error: "Database error" });
@@ -19,3 +20,4 @@ export default async function handler(req, res) {
 
   return res.status(405).json({ error: "Method not allowed" });
 }
+
