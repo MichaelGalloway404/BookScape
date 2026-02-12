465cda4b7441ee93e70e443efd115d71dff4471a
MichaelGalloway404 <michael.d.galloway404@gmail.com>
2026-02-12 11:54:01 -0600

    list out avalible users

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,16 +19,23 @@ function Home() {
 
             setUser(data);
         };
-        loadUser(); 
+        loadUser();
     }, []);
 
     return (
         <div>
-            {/* some ugly debug info, delete later */}
-            <p><strong>Debug ID:</strong> {user.id}</p>
-            <p><strong>Debug Username:</strong> {user.username}</p>
-            <p><strong>Debug Book Order:</strong> {user.book_order_json}</p>
+
             <h1>Home Page</h1>
+            <ul style={{ display: "flex", gap: "1rem", flexWrap: "wrap" }}>
+                {user.map((person, i) => (
+                    <li key={i} style={{ listStyle: "none" }}>
+                        {/* some ugly debug info, delete later */}
+                        <p><strong>Debug ID:</strong> {person.id}</p>
+                        <p><strong>Debug Username:</strong> {person.username}</p>
+                        <p><strong>Debug Book Order:</strong> {person.book_order_json}</p>
+                    </li>
+                ))}
+            </ul >
             <button onClick={() => navigate("/login")}>
                 Login
             </button>
