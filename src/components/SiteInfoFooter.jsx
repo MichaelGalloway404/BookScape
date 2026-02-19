9cd77c3b93703465956ddfd86d0b13dee16fcbfb
MichaelGalloway404 <michael.d.galloway404@gmail.com>
2026-02-19 11:59:19 -0600

    reset when home button pressed

--- src/components/SiteInfoFooter.jsx
+++ src/components/SiteInfoFooter.jsx
@@ -1,5 +1,6 @@
 import styles from './SiteInfoFooter.module.css'
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 function SiteInfoFooter(props) {
   const today = new Date();
@@ -7,6 +8,10 @@ function SiteInfoFooter(props) {
   const date = today.getDate();
   const year = today.getFullYear();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    document.body.style.background = "linear-gradient(135deg, #0f2027, #203a43, #2c5364)";
+  }, []);
 
   return (
     <>
