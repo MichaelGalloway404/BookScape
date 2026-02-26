9b469a0e934b8cd5ff33c4165656bacf308d5b2a
MichaelGalloway404 <michael.d.galloway404@gmail.com>
2026-02-26 10:49:43 -0600

    added user control trigger

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -22,32 +22,34 @@ function BookList({
 
   return (
     <>
-      <div style={{ display: "flex", gap: "0.5rem" }}>
-        <input
-          type="color"
-          value={bgColor}
-          onChange={(e) => setBgColor(e.target.value)}
-        />
+      {editMode && (
+        <div style={{ display: "flex", gap: "0.5rem" }}>
+          <input
+            type="color"
+            value={bgColor}
+            onChange={(e) => setBgColor(e.target.value)}
+          />
 
-        <input
-          style={{ height: "20px", width: "50px" }}
-          type="number"
-          min="1"
-          value={borderSize}
-          onChange={(e) => setBorderSize(Number(e.target.value))}
-        />
+          <input
+            style={{ height: "20px", width: "50px" }}
+            type="number"
+            min="1"
+            value={borderSize}
+            onChange={(e) => setBorderSize(Number(e.target.value))}
+          />
 
-        <input
-          type="color"
-          value={borderColor}
-          onChange={(e) => setBorderColor(e.target.value)}
-        />
-        <input
-          type="color"
-          value={pageBckColor}
-          onChange={(e) => setPageBckColor(e.target.value)}
-        />
-      </div>
+          <input
+            type="color"
+            value={borderColor}
+            onChange={(e) => setBorderColor(e.target.value)}
+          />
+          <input
+            type="color"
+            value={pageBckColor}
+            onChange={(e) => setPageBckColor(e.target.value)}
+          />
+        </div>
+      )}
       <ul style={{ display: "flex", gap: "1rem", flexWrap: "wrap" }}>
         {books.map((book, i) => (
           <DraggableBookCard
