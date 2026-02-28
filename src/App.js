97eda83d671f806469bb0e93cc8dff318ff7280e
MichaelGalloway404 <michael.d.galloway404@gmail.com>
2026-02-28 11:33:20 -0600

    removed some unneeded test pages

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import BookSearch from "./pages/BookSearch"
 import SignupPage from "./pages/SignupPage";
 import PublicPages from "./pages/PublicPages"
-import ColorPalettePicker from "./components/ColorPalettePicker"
 
 function App() {
   return (
@@ -19,7 +18,6 @@ function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signUp" element={<SignupPage />} />
       <Route path="/publicPage" element={<PublicPages />} />
-      <Route path="/colorTester" element={<ColorPalettePicker/>} />
 
       <Route path="/second" element={
           <ProtectedRoute>
