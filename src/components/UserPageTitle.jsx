c7b0bdffad2efd641b12acde0ffbd64fe20be269
MichaelGalloway404 <michael.d.galloway404@gmail.com>
2026-03-02 14:42:16 -0600

    testing edit hover

--- src/components/UserPageTitle.jsx
+++ src/components/UserPageTitle.jsx
@@ -56,6 +56,7 @@ function UserPageTitle({ editMode, settings, setSettings, titlePlaceHolder }) {
         <>
             {/* Bio display */}
             <h1
+                className={editMode ? "hover:shadow-lg transition-shadow duration-200" : ""}
                 style={{
                     fontFamily,
                     background: bgColor,
