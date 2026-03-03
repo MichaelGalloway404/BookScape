1758f202bcc4bfb5efdcbebddf81561ae416a440
MichaelGalloway404 <michael.d.galloway404@gmail.com>
2026-03-03 11:28:14 -0600

    added pop up component to main page

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -95,40 +95,51 @@ function BookList({
   return (
     <>
       {editMode && (
-        <div style={{ display: "flex", gap: "0.5rem" }}>
-          <p>BookCard Color</p>
-          <input
-            type="color"
-            value={bgColor}
-            onChange={(e) => setBgColor(e.target.value)}
-          />
+        <EditablePopup
+          popupRef={popupRef}
+          controls={{
+            bgColor: [bgColor, setBgColor],
+            borderColor: [borderColor, setBorderColor],
+            borderSize: [borderSize, setBorderSize],
+            pageBckColor: [pageBckColor, setPageBckColor],
+            
+          }}
+        />
+        // <div style={{ display: "flex", gap: "0.5rem" }}>
+        //   <p>BookCard Color</p>
+        //   <input
+        //     type="color"
+        //     value={bgColor}
+        //     onChange={(e) => setBgColor(e.target.value)}
+        //   />
 
-          <p>Border Size</p>
-          <input
-            style={{ height: "20px", width: "50px" }}
-            type="number"
-            min="0"
-            value={borderSize}
-            onChange={(e) => setBorderSize(Number(e.target.value))}
-          />
+        //   <p>Border Size</p>
+        //   <input
+        //     style={{ height: "20px", width: "50px" }}
+        //     type="number"
+        //     min="0"
+        //     value={borderSize}
+        //     onChange={(e) => setBorderSize(Number(e.target.value))}
+        //   />
 
-          <p>BookCard Border Color</p>
-          <input
-            type="color"
-            value={borderColor}
-            onChange={(e) => setBorderColor(e.target.value)}
-          />
+        //   <p>BookCard Border Color</p>
+        //   <input
+        //     type="color"
+        //     value={borderColor}
+        //     onChange={(e) => setBorderColor(e.target.value)}
+        //   />
 
-          <p>Page Background Color</p>
-          <input
-            type="color"
-            value={pageBckColor}
-            onChange={(e) => setPageBckColor(e.target.value)}
-          />
-        </div>
+        //   <p>Page Background Color</p>
+        //   <input
+        //     type="color"
+        //     value={pageBckColor}
+        //     onChange={(e) => setPageBckColor(e.target.value)}
+        //   />
+        // </div>
       )}
       <ul style={{ display: "flex", gap: "1rem", flexWrap: "wrap" }}>
         {books.map((book, i) => (
+          //  allows for a book card to be rearranged and for a book wiki API summary
           <DraggableBookCard
             key={`${book.isbn}-${book.cover_id}`}
             book={book}
