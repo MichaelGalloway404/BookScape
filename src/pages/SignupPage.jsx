2bdf67ccaaaf97d8a9ee634b9679eeafeb8741de
MichaelGalloway404 <michael.d.galloway404@gmail.com>
2026-03-08 17:07:33 -0500

    removed sign up page deafult user settings on profile creation

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import style from "./SignupPage.module.css";
 import SiteInfoFooter from '../components/SiteInfoFooter';
-import axios from "axios";
 
 function SignupPage() {
   const navigate = useNavigate();
@@ -27,36 +26,6 @@ function SignupPage() {
 
       if (!res.ok) {
         throw new Error(data.error || "Signup failed");
-      }
-
-      // put generic default settings to make user visual look profile
-      const settings = {
-        "UserBio": {
-          "text": "Type your Bio here...Or turn it off!", "bgColor": "#f8e2af", "padding": 5, "bgColor2": "#d4d4d4", 
-          "fontSize": 40, "displayOn": true, "fontColor": "black", "marginTop": 7, "borderSize": 5, "fontFamily": "Arial", 
-          "marginLeft": 1, "borderColor": "#ecd6ac", "borderStyle": "ridge", "marginRight": 5, "borderRadius": 5, "marginBottom": 6, 
-          "gradientAngle": 135}, 
-        "bookCard": {
-          "margin": 1, "bgColor": "#9fe8c1", "padding": 10, "bgColor2": "#cfe3b5", "titleSize": 20, "authorSize": 20, 
-          "borderSize": 2, "titleColor": "#fc5050", "titleWidth": 100, "authorColor": "#676767", "authorWidth": 100, 
-          "borderColor": "#dae7b1", "borderStyle": "solid", "titleMargin": 1, "authorMargin": 1, "borderRadius": 5, "cardImgWidth": 100, 
-          "titlePadding": 5, "authorPadding": 5, "gradientAngle": 135, "cardImgBorderSize": 2, "cardImgBorderColor": "#ffffff", 
-          "cardImgBorderStyle": "solid", "cardImgBorderRadius": 5}, 
-        "mainPage": {"pageBckColor": "#deebad", "gradientAngle": 135, "pageBckColor2": "#38d64d"}, 
-        "UserPageTitle": {"text": "Make a page Title", "bgColor": "#ffaaaa", "padding": 5, "bgColor2": "#ff8484", 
-          "fontSize": 40, "displayOn": true, "fontColor": "#efefef", "marginTop": 1, "borderSize": 4, "fontFamily": "Arial", 
-          "marginLeft": -20, "borderColor": "#b3dfe6", "borderStyle": "solid", "marginRight": 3, "borderRadius": 5, 
-          "marginBottom": 12, "gradientAngle": 135}
-      }
-      try {
-        await axios.post(
-          "/api/userSettings",
-          settings,
-          { withCredentials: true }
-        );
-      } catch (err) {
-        console.error(err);
-        alert("Failed to Save Default Settings!");
       }
 
       // success → navigate to login
@@ -105,7 +74,7 @@ function SignupPage() {
           Already have an account?{" "}
           <span onClick={() => navigate("/login")}>Sign in</span>
         </p>
-        <SiteInfoFooter />
+        <SiteInfoFooter/>
       </div>
     </div>
   );
